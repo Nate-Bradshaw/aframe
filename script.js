29533cfdb143fff0d76b3e049ad340a1e034412e
Nayt <nate.bradshaw5959@gmail.com>
2026-03-30 00:36:51 +0100

    i know how to make components now

---
+++ script.js
@@ -0,0 +1,13 @@
+//https://aframe.io/docs/1.7.0/introduction/javascript-events-dom-apis.html
+
+//we want to do almost all the coding in componnts (see log.js for a brief example)
+//if youve ever used unity, the docs explicity state that its ECS is very simular.
+
+
+var sceneEl = document.querySelector("a-scene"); //grabbing scene object
+console.log(sceneEl);
+var el = sceneEl.querySelector("#TestBox"); //grabbing an entity by tag
+//var camera = document.querySelector('a-entity[camera]').components.camera.camera;
+
+console.log(el)
+console.log(el.position)
