fcc247d1e109a3e0da292766c06aa82ad49ffb79
Nayt <nate.bradshaw5959@gmail.com>
2026-03-30 23:54:43 +0100

    beats can fly from far onto the circle

--- components/beat_anim.js
+++ components/beat_anim.js
@@ -8,9 +8,9 @@ AFRAME.registerComponent('beat_anim', {
     
     //see property types in component docs, can do custom types
     schema: {
-        startPos: {type: 'vec3'},
         ring: {type: 'selector'}, //this should be the ring where the notes need to go
-        angle: {type: 'number'} //float
+        start_depth: {type: 'number'},
+        angle: {type: 'number'}, //float
         //note on angle, this will be what we want the LOCAL angle to be
         //global angle calculations will need to be made
         //this.data.startPos = {0,1,2}
@@ -18,11 +18,28 @@ AFRAME.registerComponent('beat_anim', {
     },
 
     init: function () {
-        this.el.setAttribute("position", this.data.startPos)
+        //vectors are threejs
+        var pos = new THREE.Vector3(0, 0, this.data.start_depth);
+        pos.add(this.data.ring.object3D.position);
+        this.el.setAttribute("position", pos);
+        this.radius = this.data.ring.components.geometry.data.radiusInner;
+        this.deltaT = 0;
+        this.curPos = pos;
+        var x = pos.x + (this.radius * Math.cos(THREE.MathUtils.degToRad(this.data.angle)));
+        var y = pos.y + (this.radius * Math.sin(THREE.MathUtils.degToRad(this.data.angle)));
+        this.endPos = new THREE.Vector3(x, y, this.data.ring.object3D.position.z);
+
+        console.log(this.data.ring.components.geometry.data);
     },
 
     tick: function () {
-        console.log(this.data.fooI);
-        this.el.setAttribute("beatAnim", {fooI: (this.data.fooI + 1)});
+        //console.log(this.data.fooI);
+        //this.el.setAttribute("beatAnim", {fooI: (this.data.fooI + 1)});
+        var pos = this.curPos.lerp(this.endPos, 0.05);
+        //if lerp is done:
+        //create an event stating that a note has hit ring
+        //delete this object
+
+        this.el.setAttribute("position", pos)
     }
 });
