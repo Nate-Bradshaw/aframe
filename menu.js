0a6a36f942eae08a2f1a5b711ee4f552d93bf73a
c23044708 <kruszewskik@cardiff.ac.uk>
2026-04-08 20:34:41 +0100

    added filler text and made exit button bigger

--- menu.js
+++ menu.js
@@ -1,27 +1,32 @@
 window.addEventListener("load", () => {
   // Loading each UI element using its ID
-  const instuctionbtn = document.querySelector("#instruction-btn")
+  const instructionbtn = document.querySelector("#instruction-btn")
   const switchBtn = document.querySelector("#switch-button")
   const exampleMenu = document.querySelector("#example-menu")
   const exitBtn = document.querySelector("#exit-button")
   const menuTxt = document.querySelector("#menuText")
+  const instructionTxt = document.querySelector("#gameInstructions")
+
+  console.log(instructionTxt)
 
   // Start Button action
-  instuctionbtn.addEventListener("click", () => {
+  instructionbtn.addEventListener("click", () => {
+    exampleMenu.setAttribute("visible", true);
+    menuTxt.setAttribute("visible", true);
+    instructionTxt.setAttribute("visible", true);
     toggle_clickable(switchBtn, false);
-    toggle_clickable(instuctionbtn, false);
+    toggle_clickable(instructionbtn, false);
     toggle_clickable(exitBtn, true);
-    exampleMenu.setAttribute("visible", "true");
-    menuTxt.setAttribute("visible", "true");
   });
 
   // Exit Button action
   exitBtn.addEventListener("click", () => {
-    exampleMenu.setAttribute("visible", "false");
-    menuTxt.setAttribute("visible", "false");
+    exampleMenu.setAttribute("visible", false);
+    menuTxt.setAttribute("visible", false);
+    instructionTxt.setAttribute("visible", false);
     toggle_clickable(exitBtn, false);
     toggle_clickable(switchBtn, true);
-    toggle_clickable(instuctionbtn, true);
+    toggle_clickable(instructionbtn, true);
   });
 
   // Switches to the game
@@ -32,10 +37,10 @@ window.addEventListener("load", () => {
 
 function toggle_clickable(btn, state) {
   if (state) {
-    btn.setAttribute("visible", "true");
+    btn.setAttribute("visible", true);
     btn.setAttribute("class", "clickable");
   } else {
-    btn.setAttribute("visible", "false");
+    btn.setAttribute("visible", false);
     btn.setAttribute("class", "not-clickable");
   }
 }
